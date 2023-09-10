0c782b0eac7798efb1982d30ef394ea626f1b53d
yamahiro20639 <144509349+yamahiro20639@users.noreply.github.com>
2023-09-10 16:29:51 +0900

    Add files via upload

---
+++ Practice/build/Element/src/main/java/org/example/Main.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        String[] names = {"山田", "酒井", "佐々木", "田中", "佐藤", "高橋"};
+        System.out.println(names[8]);
+    }
+}
